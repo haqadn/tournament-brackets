6ea9fea54bab3aa2ba20c4db636d832f3b65df66
Mohaimenul Adnan <adnan007.id@gmail.com>
2016-05-08 17:56:31 +0600

    Added brackets styles and js.

--- js/plugin.js
+++ js/plugin.js
@@ -13,6 +13,27 @@
 				end_time: $(this).data('end-time')
 			});
 		});
+
+		//Highlight a team when hovered
+		$(".m_segment").on("mouseover mouseout",function () {
+			var $this = $(this);
+			var winnderId = $this.attr("data-team-id");
+			var $teams = $("[data-team-id="+winnderId+"]");
+			$teams.toggleClass('highlight').parent().toggleClass('highlight');
+		});
+		// highlight each round with its similar reversed round
+		function highlightRounds(roundClass){
+			roundClass.on('mouseover mouseout',function(){
+				roundClass.toggleClass('focus')
+			})
+		}
+		highlightRounds($('.r_64'));
+		highlightRounds($('.r_32'));
+		highlightRounds($('.r_16'));
+		highlightRounds($('.r_8'));
+		highlightRounds($('.r_4'));
+		highlightRounds($('.r_2'));
+
 	});
 
 	$(window).load(function(){
